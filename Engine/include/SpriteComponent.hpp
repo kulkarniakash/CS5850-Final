9ba92f683f1bc86c24433de240f3c3f301109bf9
Kimin Lee <kiminlee2000225@gmail.com>
2021-04-19 18:04:56 -0400

    addAnimation and performAnimation
    in SpriteComponent

--- Engine/include/SpriteComponent.hpp
+++ Engine/include/SpriteComponent.hpp
@@ -2,6 +2,7 @@
 #define SPRITE_COMPONENT_HPP
 #include <stdio.h>
 #include <memory>
+#include <map>
 #include <SDL_Headers.hpp>
 #include <Vec2.hpp>
 #include <TransformComponent.hpp>
@@ -17,9 +18,12 @@ public:
     
     //destructor fpr SpriteComponent
     ~SpriteComponent();
+
+
+    void addAnimation(const std::string& animationName, const int& startFrame, const int& endFrame);
     
     //allows that sprite to perform an animation
-    void performAnimation(const std::string & animationName, int frame);
+    void performAnimation(const std::string & animationName, const bool& isFlipped);
     
     //renders our sprite
     void render();
@@ -31,7 +35,12 @@ public:
     
 
 private:
+    std::map<std::string, std::pair<int, int>> m_animations;
+    std::pair<int, int> m_frameRange;
     int m_frame = 0;
+    int m_animationSpeed;
+    std::string m_currentAction;
+    bool m_flipped;
 
 	SDL_Rect m_src;
 	SDL_Rect m_dest;
