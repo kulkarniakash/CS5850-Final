bd422bc9074f4df4ed25c182d07d0461e8347bd2
Kimin Lee <kiminlee2000225@gmail.com>
2021-04-20 22:26:52 -0400

    Low level sprite animation with the controlller
    Just checking if they work together

--- Engine/main.py
+++ Engine/main.py
@@ -67,7 +67,6 @@ biden.add_controller_component(control)
 ##tran = Engine.TransformComponent(Engine.Vec2(0,0), Engine.Vec2(5,10))
 ##obj.add_transform_component(tran)
 
-<<<<<<< 396266f02c000fe4060401eda44367e44380c75b
 class Character(Engine.PlayerObject):
     def __init__(self, name):
         super().__init__(name)
@@ -135,45 +134,10 @@ character.update_animation_idle(True, 3)
 
 #engine.add_player_object(biden)
 engine.add_player_object(character)
-=======
-##engine.add_player_object(biden)
-##engine.add_game_object(sky)
-##engine.add_UF_callback(gravity)
-
-class Character(Engine.GameObject):
-    def __init__(self, name):
-        super().__init__(name)
-
-    def character_sprite_init(self):
-        dest = Engine.Rect()
-        src = Engine.Rect()
-        dest.x , dest.y, dest.w, dest.h = 0, 0, 100, 200
-        src.x, src.y, src.w, src.h = 0, 0, 50, 37
-        rows, cols = 16, 7
-        self.character_sprite = Engine.CharacterSpriteComponent("./assets/adventurer.jpg", dest, src, rows, cols)
-        self.character_sprite.add_animation("idle", 0, 3)
-        super().add_character_sprite_component(self.character_sprite)
-        
-    def update_animation(self):
-        self.character_sprite.perform_animation("idle", True)
-
-character = Character("character")
-character.character_sprite_init()
-character.update_animation()
-
-tran2 = Engine.TransformComponent(Engine.Vec2(0,0), Engine.Vec2(0, 0))
-character.add_transform_component(tran2)
-
-engine.add_game_object(sky)
-engine.add_player_object(character)
-
->>>>>>> Character Sprite Component with example of adventurer.jpg for idle motion
 engine.start()
 
 while not engine.program_ended():
     engine.input()
-    #sky.update()
-    #character.update()
     engine.update()
 ##    sky.update()
 ##    character.update()
