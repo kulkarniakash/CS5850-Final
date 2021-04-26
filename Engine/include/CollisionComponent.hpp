c29ab46ca21567987f79ff80a0b29834ed6def0a
Danny Son <dannyson900@gmail.com>
2021-04-25 23:49:20 -0400

    Added documentation to class and functions.

--- Engine/include/CollisionComponent.hpp
+++ Engine/include/CollisionComponent.hpp
@@ -5,12 +5,26 @@
 #include <vector>
 class AnimateObject;
 
+/*!
+ *  @brief class that represents the Colloision Component of our GameObject
+ *  Handles Collisions of our GameObject
+ */
 class CollisionComponent {
 public:
+    
+    /*!
+     *  @brief Constructor of our CollisionComponent
+     *  @param obj: AnimateObject that we want to detect colloision for
+     */
 	CollisionComponent(AnimateObject* obj);
 
+    //! @brief Destructor of our CollisionComponent
 	~CollisionComponent();
-
+    
+    /*!
+     *  @brief handles the collision of our Object
+     *  @param objs: A vector of game objects we want to test collisions against
+     */
 	void handleCollisions(std::vector<GameObject*> objs);
 
 private:
